8ef4adc8d99125c64c45b6468bd07e3a83497c51
Santiago Salcedo <121301997+SantiagoSalcedoElevation@users.noreply.github.com>
2024-09-13 18:53:45 -0300

    change : image gallery

--- wp-content/themes/elevation-framework/src/blocks/interior-components/image-gallery/edit.js
+++ wp-content/themes/elevation-framework/src/blocks/interior-components/image-gallery/edit.js
@@ -1,63 +1,155 @@
 /* Gutenberg Dependencies */
 import { __ } from '@wordpress/i18n';
-import { InnerBlocks, useBlockProps } from '@wordpress/block-editor';
-import { __experimentalInputControl } from '@wordpress/components';
-import { useEffect } from '@wordpress/element';
+import {
+	useBlockProps,
+	BlockControls,
+	MediaReplaceFlow,
+	MediaPlaceholder,
+	URLInput,
+	InspectorControls,
+} from '@wordpress/block-editor';
+import {
+	Button,
+	Popover,
+	ToggleControl,
+	PanelBody,
+} from '@wordpress/components';
+import { keyboardReturn } from '@wordpress/icons';
+import { useState, useEffect } from '@wordpress/element';
 
 /* Internal Dependencies */
 import { getBlockName } from '../../utils/helpers';
 
 /* Block */
-import './editor.scss';
 import json from './block.json';
-import previewImage from './preview.webp';
-import { allowedBlocks, TEMPLATE } from './template';
+import './editor.scss';
 
 const Edit = (props) => {
 	const { name: blockName } = json;
 	const { name, blockId } = getBlockName(blockName);
 
-	const { clientId, attributes, setAttributes } = props;
-
-	const { id, anchor, preview } = attributes;
+	const { attributes, setAttributes, context } = props;
+	const { img, link, isLazy, showCaption } = attributes;
+	const inheritClassName = context['elevation/image-classname'];
 
 	useEffect(() => {
-		setAttributes({ id: `${name}-${clientId}` });
-	}, [clientId, name, setAttributes]);
+		setAttributes({
+			inheritClassName: inheritClassName || 'media',
+		});
+	}, [inheritClassName, setAttributes]);
 
 	const blockProps = useBlockProps({
-		className: `${name} swiper-slide`,
+		className: `${name} ${inheritClassName}`,
 	});
 
-	if (preview) {
-		return (
-			<div className={`${name}-preview`}>
-				<img
-					src={previewImage}
-					alt="Preview"
-					style={{
-						objectFit: 'contain',
-						maxHeight: '100%',
-						maxWidth: '100%',
+	const setImageAttributes = (media) => {
+		const { url, id, caption } = media;
+
+		setAttributes({
+			img: {
+				url,
+				id,
+				caption,
+			},
+		});
+	};
+
+	const [visible, setVisible] = useState(false);
+
+	const controls = (
+		<InspectorControls>
+			<PanelBody title={__('Image Settings')}>
+				<ToggleControl
+					__nextHasNoMarginBottom
+					label="Defer image loading?"
+					help={isLazy ? 'Yes' : 'No'}
+					checked={isLazy}
+					onChange={(newValue) => {
+						setAttributes({
+							isLazy: newValue,
+						});
 					}}
 				/>
-			</div>
-		);
-	}
+			</PanelBody>
+		</InspectorControls>
+	);
+
+	const blockControls = (
+		<BlockControls>
+			<MediaReplaceFlow
+				mediaId={img?.id}
+				mediaUrl={img?.url}
+				allowedTypes={['image']}
+				onSelect={setImageAttributes}
+				name={!img?.url ? __('Add Image') : __('Replace Image')}
+			/>
+			<Button variant="secondary" onClick={() => setVisible(!visible)}>
+				{link.url ? 'Change Link' : 'Add Link to Image'}
+			</Button>
+			{visible && (
+				<Popover onClose={() => setVisible(false)}>
+					<form className={`${name}__form`}>
+						<URLInput
+							value={link.url}
+							onChange={(value) => {
+								setAttributes({
+									link: {
+										...link,
+										url: value,
+									},
+								});
+							}}
+						/>
+						<Button
+							onClick={() => setVisible(false)}
+							icon={keyboardReturn}
+							label={__('Apply')}
+							type="submit"
+						/>
+					</form>
+					<ToggleControl
+						label={__('Open in new tab')}
+						checked={link.target}
+						onChange={(value) => {
+							setAttributes({
+								link: {
+									...link,
+									target: value,
+								},
+							});
+						}}
+					/>
+				</Popover>
+			)}
+		</BlockControls>
+	);
 
 	return (
 		<>
-			<div
-				data-block-id={blockId}
-				data-block-js="true"
-				id={anchor || id}
-				{...blockProps}
-			>
-				<InnerBlocks
-					template={TEMPLATE}
-					allowedBlocks={allowedBlocks}
-					templateLock={'all'}
-				/>
+			{controls}
+			<div {...blockProps}>
+				{blockControls}
+				{img?.url ? (
+					<>
+						<img
+							src={img?.url}
+							className={`${name}__img`}
+							loading={isLazy ? 'lazy' : 'eager'}
+						/>
+						{img?.caption && (
+							<span className={`${name}__img--caption`}>
+								{img.caption}
+							</span>
+						)}
+					</>
+				) : (
+					<MediaPlaceholder
+						onSelect={setImageAttributes}
+						allowedTypes={['image']}
+						multiple={false}
+						labels={{ title: 'Image' }}
+					/>
+				)}
 			</div>
 		</>
 	);
