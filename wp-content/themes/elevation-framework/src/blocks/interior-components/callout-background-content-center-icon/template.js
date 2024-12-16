8c3cc1f9bc687b82997060b412080fb27866d3d9
Facundo Chico <facundo.chico@elevationweb.org>
2024-12-16 17:25:06 -0300

    Callout - centered Content with Icon

---
+++ wp-content/themes/elevation-framework/src/blocks/interior-components/callout-background-content-center-icon/template.js
@@ -0,0 +1,77 @@
+export const template = [
+	[
+		'elevation/interior-components--custom-container',
+		{
+			className:
+				'callout-background-content-center-icon__custom-container',
+			containerWidth: 'medium',
+			backgroundColor: 'ui-foreground-primary-text-headings-span',
+		},
+		[
+			[
+				'elevation/interior-components--group',
+				{
+					className: '',
+				},
+				[
+					[
+						'elevation/interior-components--image-with-srcset',
+						{
+							img: {
+								url: `/wp-content/themes/elevation-framework/src/blocks/interior-components/callout-background-content-center-icon/images/callout-icon.webp`,
+							},
+							layout: { type: 'flex', justifyContent: 'center' },
+							align: 'center',
+						},
+					],
+					[
+						'core/heading',
+						{
+							level: 2,
+							content: 'H2. Heading lorem',
+							textColor: 'ui-base-white',
+							textAlign: 'center',
+							align: 'center',
+						},
+					],
+					[
+						'core/paragraph',
+						{
+							fontSize: 'body',
+							textColor: 'ui-base-white',
+							content: `Maximum 30 words. Nisi est sit amet facilisis magna etiam tempor orci eu lobortis elementum nibh tellus molestie nunc non blandit massa enim nec dui nunc mattis enim ut tellus elementu..`,
+							textAlign: 'center',
+							align: 'center',
+						},
+					],
+					[
+						'elevation/interior-components--buttons',
+						{ layout: { type: 'flex', justifyContent: 'center' } },
+						[
+							[
+								'elevation/interior-components--button',
+								{
+									btnStyle: 'primary-inverse',
+									text: 'Text Button',
+								},
+							],
+							[
+								'elevation/interior-components--button',
+								{
+									btnStyle: 'secondary-inverse',
+									text: 'Text Button',
+								},
+							],
+						],
+					],
+				],
+			],
+		],
+	],
+];
+
+export const allowedBlocks = [
+	'core/heading',
+	'core/paragraph',
+	'elevation/interior-components--buttons',
+];
