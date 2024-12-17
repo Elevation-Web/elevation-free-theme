975142f6ebb6d7dedffe2bdbd565e3959dc597d0
Facundo Chico <facundo.chico@elevationweb.org>
2024-12-17 11:49:54 -0300

    Rebase

--- wp-content/themes/elevation-framework/build/blocks/interior-components/impact-default/index.asset.php
+++ wp-content/themes/elevation-framework/build/blocks/interior-components/impact-default/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'fa6b0ad7a2ffdb02eab1');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '8c423cd9b2d5df155a5b');
--- wp-content/themes/elevation-framework/build/blocks/interior-components/spacer/index.asset.php
+++ wp-content/themes/elevation-framework/build/blocks/interior-components/spacer/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => '36a95948cb40f8c2e07f');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => 'be0926330bc82d41f1f5');
