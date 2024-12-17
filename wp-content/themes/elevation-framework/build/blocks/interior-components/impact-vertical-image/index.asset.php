ffdddb81f8a6d2326cfe07ed2aa53b50856d772c
Facundo Chico <facundo.chico@elevationweb.org>
2024-12-17 12:35:38 -0300

    Rebase

--- wp-content/themes/elevation-framework/build/blocks/interior-components/impact-vertical-image/index.asset.php
+++ wp-content/themes/elevation-framework/build/blocks/interior-components/impact-vertical-image/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '12894c3074b84619d379');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '2434788cc42bc0ad13bd');
