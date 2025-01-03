5fa4c9f73152d64b70cfa14ecc745e9faa841f17
SantiagoSalcedoElevation <santiago.salcedo@elevationweb.org>
2025-01-03 15:14:12 -0300

    rebase

--- wp-content/themes/elevation-framework/build/blocks/interior-components/impact-default-content-centered/index.asset.php
+++ wp-content/themes/elevation-framework/build/blocks/interior-components/impact-default-content-centered/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '984e31b80794365e6834');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'd3b01cce7bbdc2dcbbd1');
