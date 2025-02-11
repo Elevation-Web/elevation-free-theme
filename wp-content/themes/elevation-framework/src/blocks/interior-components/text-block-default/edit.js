de9fe24ca387d36d58b7d2107a0b8c3608951657
msuarez25 <mauricio.suarez25@gmail.com>
2025-02-11 09:35:42 -0500

    Text block, Layout: Default

---
+++ wp-content/themes/elevation-framework/src/blocks/interior-components/text-block-default/edit.js
@@ -0,0 +1,59 @@
+import { __ } from '@wordpress/i18n';
+import { useBlockProps, InnerBlocks, RichText } from '@wordpress/block-editor';
+
+import { useEffect } from '@wordpress/element';
+
+/* Internal Dependencies */
+import previewImage from './preview.webp';
+import { allowedBlocks, template } from './template';
+import { getBlockName } from '../../utils/helpers';
+
+/* Block */
+import json from './block.json';
+
+const Edit = (props) => {
+	const { name: blockName } = json;
+	const { name } = getBlockName(blockName);
+
+	const { clientId, attributes, setAttributes } = props;
+
+	const { id, anchor, preview } = attributes;
+
+	useEffect(() => {
+		setAttributes({ id: `${name}-${clientId}` });
+	}, [clientId, name, setAttributes]);
+
+	const blockProps = useBlockProps({
+		className: `${name}`,
+	});
+
+	if (preview) {
+		return (
+			<div className={`${name}-preview`}>
+				<img
+					src={previewImage}
+					alt="Preview"
+					style={{
+						objectFit: 'contain',
+						maxHeight: '100%',
+						maxWidth: '100%',
+					}}
+				/>
+			</div>
+		);
+	}
+
+	return (
+		<>
+			<div id={anchor || id} {...blockProps}>
+				<InnerBlocks
+					template={template}
+					allowedBlocks={allowedBlocks}
+					templateLock={false}
+				/>
+			</div>
+		</>
+	);
+};
+
+export default Edit;
