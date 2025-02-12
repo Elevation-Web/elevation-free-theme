df9717acf255e87ae29b2b7331de780a4ed8ff5a
msuarez25 <mauricio.suarez25@gmail.com>
2025-02-12 13:15:00 -0500

    Text Block, Layout: Quote

--- wp-content/themes/elevation-framework/build/assets/index.asset.php
+++ wp-content/themes/elevation-framework/build/assets/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime'), 'version' => '9151d9d6030ff373697e');
+<?php return array('dependencies' => array('react-jsx-runtime'), 'version' => '70892dde4b544311b2f8');
