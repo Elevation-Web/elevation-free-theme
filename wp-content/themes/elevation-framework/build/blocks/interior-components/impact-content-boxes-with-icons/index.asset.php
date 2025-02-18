ad2cd4f66b99600ccd8e9b6fbfb6a3622ee9d0ae
msuarez25 <mauricio.suarez25@gmail.com>
2025-02-18 12:50:13 -0500

    Remove option to add text

--- wp-content/themes/elevation-framework/build/blocks/interior-components/impact-content-boxes-with-icons/index.asset.php
+++ wp-content/themes/elevation-framework/build/blocks/interior-components/impact-content-boxes-with-icons/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'b512e0d463dfc6dca32f');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '2a47fe966b1657ed05cc');
