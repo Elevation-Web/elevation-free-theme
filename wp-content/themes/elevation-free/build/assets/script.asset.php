abbaa7ebd3f81d061f5742d1e2aed736c5ba2050
msuarez25 <mauricio.suarez25@gmail.com>
2025-04-08 13:55:46 -0400

    fixed format

--- wp-content/themes/elevation-free/build/assets/script.asset.php
+++ wp-content/themes/elevation-free/build/assets/script.asset.php
@@ -1,1 +1,4 @@
-<?php return array('dependencies' => array(), 'version' => 'faffecf10b736221e734');
+<?php return array(
+	'dependencies' => array(),
+	'version'      => 'faffecf10b736221e734',
+);
--- wp-content/themes/elevation-free/build/single/index.asset.php
+++ wp-content/themes/elevation-free/build/single/index.asset.php
@@ -1,1 +1,4 @@
-<?php return array('dependencies' => array(), 'version' => '49b3a2b17265df9a2499');
+<?php return array(
+	'dependencies' => array(),
+	'version'      => '49b3a2b17265df9a2499',
+);
